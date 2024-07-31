7fbdbab971c5f7ee2a86db91216b6523ba121a0b
Soukaina111 <basmahajar@gmail.com>
2024-07-31 15:09:37 +0000

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 0. Regex-ing """
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    # Create regex pattern to match fields
+    pattern = r'|'.join(rf'({field})=\w+' for field in fields)
+    # Replace matched fields with redaction
+    return re.sub(pattern, lambda m: f'{m.groups()[0]}={redaction}', message)
