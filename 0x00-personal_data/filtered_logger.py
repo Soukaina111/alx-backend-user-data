1212f5e5506dc0fa164603322841129d6bfa0b99
Soukaina111 <basmahajar@gmail.com>
2024-07-31 16:12:23 +0000

    4-filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,7 @@ import logging
 import mysql.connector
 import os
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ Create regex pattern to match fields and
