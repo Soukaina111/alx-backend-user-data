7447c4fcffeb7eb0b003cba8271f2f8697978869
Soukaina111 <basmahajar@gmail.com>
2024-07-31 16:24:24 +0000

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Utility functions for hashing and verifying passwords using bcrypt.
+"""
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes the provided password using bcrypt.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        bytes: The salted, hashed password as a byte string.
+    """
+    # Encode the password to bytes before hashing
+    password_bytes = password.encode('utf-8')
+    # Generate a salt and hash the password using bcrypt
+    hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates the provided password against the given hashed password.
+    Returns:
+        bool: True if the provided password matches the hashed password, False otherwise.
+    """
+    # Encode the password to bytes before checking
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
