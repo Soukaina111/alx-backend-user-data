dbc689f6d18c8f2835e0b69d593a78e38ac993bd
Soukaina111 <basmahajar@gmail.com>
2024-07-31 15:37:15 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,11 @@
 """ 0. Regex-ing """
 
 import re
+from typing import List, Tuple, Callable, Match
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
     # Create regex pattern to match fields
     pattern = r'|'.join(rf'({field})=\w+' for field in fields)
     # Replace matched fields with redaction
