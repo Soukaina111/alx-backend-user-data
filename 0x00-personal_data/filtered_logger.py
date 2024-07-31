0b9dc0aaeb7d45cb02875fa9bdfd0779491fe5e5
Soukaina111 <basmahajar@gmail.com>
2024-07-31 15:44:33 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ # Create regex pattern to match fields and 
+    """ Create regex pattern to match fields and
     Replace matched fields with redaction"""
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
