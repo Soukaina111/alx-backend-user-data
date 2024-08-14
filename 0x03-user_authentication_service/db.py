8a8c012d888b4e3779af2adea5473caad5ecd8a9
Soukaina111 <basmahajar@gmail.com>
2024-08-14 12:00:36 +0000

    Task 1.1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,11 +3,9 @@
 DB module for database operations
 """
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from user import Base, User
 
 from user import Base
 
